36743b9866256f7d0b66ff1fb3326fadd93c827d
ian otieno <ianotieno23@gmail.com>
2025-02-24 17:39:19 +0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,29 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: MyHome()
+    ));
+}
+class MyHome extends StatelessWidget {
+  const MyHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text('My First App'),
+        title: Text('My test App'),
          backgroundColor: Colors.amber,
       ),
     
       body: Center(
-        child: Text('Hello World!'),
+        child: Image(image: 
+        NetworkImage('https://img.freepik.com/free-photo/night-sky-with-planets-galaxies-scene-generative-ai_188544-7873.jpg?t=st=1740407840~exp=1740411440~hmac=087783d5f186de8d64138e025d97065d236c8b9b7d437e74105ef0003954dff7&w=996')),
+      
       ),
       floatingActionButton: FloatingActionButton(
       onPressed: () {  },
-      child: Icon(Icons.add_a_photo),
+      child: Icon(Icons.add),
     ),
-  )));
+  );
+  }
 }
-
